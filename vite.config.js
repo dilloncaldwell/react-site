90381925e2bb7602798629333d0ba0d4e5ab5bcc
Dillon Caldwell <dcaldwell1228@gmail.com>
2025-05-03 23:08:36 -0400

    edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import markdown from 'vite-plugin-markdown';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), markdown.plugin()],
-  base: '/react-site/',
+  base: '/react-site',
 });
